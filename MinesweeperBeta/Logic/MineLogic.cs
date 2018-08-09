fcbbc5964f6fbfe5c4cf3be87b2a54d5b9f253de
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-08-09 22:08:54 +0100

    0.1 release, using Grid model - Create icon - Playable using Grid model from TextMinesweeper project - Support for Fluent Design - Display flags available and bombs on field

---
+++ MinesweeperBeta/Logic/MineLogic.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperBeta.Logic
+{
+    class GameGrid
+    {
+        public int BombQuantity { get; private set; }
+        public int VisitedCells { get; private set; }
+        public int GridWidth { get; private set; }
+        public int GridHeight { get; private set; }
+
+        public int FlagsAvailable { get; private set; }
+
+        // -1: Bomb
+        //  0: Not a bomb, nor adjacent to a bomb
+        // >0: Adjacent to at least one bomb
+        private int[,] bombsAndValues;
+
+        // -1: Flagged cell
+        //  0: Unvisited cell
+        //  1: Visited cell (once visited, cannot be flagged or unvisited)
+        private int[,] visitedPoints;
+
+        public GameGrid(int width, int height, int bombQuantity)
+        {
+            this.BombQuantity = bombQuantity;
+            this.FlagsAvailable = bombQuantity;
+            this.GridWidth = width;
+            this.GridHeight = height;
+
+            //Cannot proceed if there are meant to be more bombs than cells
+            if (bombQuantity > (GridHeight * GridWidth)) throw new Exception();
+
+            StartNewGame();
+        }
+
+        public void StartNewGame()
+        {
+            bombsAndValues = new int[GridWidth, GridHeight];
+            visitedPoints = new int[GridWidth, GridHeight];
+
+            Random bombRandom = new Random();
+            for (int i = 0; i < BombQuantity; i++)
+            {
+                //Since Random.Next() is min inclusive but max exclusive,
+                //only non-negative values less than gridWidth or gridHeight will be generated
+                int bombX = bombRandom.Next(0, GridWidth);
+                int bombY = bombRandom.Next(0, GridHeight);
+
+                //Guarantees the number of bombs desired will appear
+                //and a bomb will not be placed in the same cell more
+                //than once
+                while (bombsAndValues[bombX, bombY] == -1)
+                {
+                    bombX = bombRandom.Next(0, GridWidth);
+                    bombY = bombRandom.Next(0, GridHeight);
+                }
+
+                bombsAndValues[bombX, bombY] = -1;
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        int positionX = bombX + x - 1;
+                        int positionY = bombY + y - 1;
+                        //Values must be in grid range (0 or gridWidth/Height cause issues)
+                        if (positionX <= GridWidth - 1 && positionY <= GridHeight - 1 && positionX >= 0 && positionY >= 0)
+                        {
+                            //For all cells adjacent to a bomb that are not bombs,
+                            //add one to the value of bombs that cell is adjacent to
+                            if (bombsAndValues[positionX, positionY] > -1) bombsAndValues[positionX, positionY] += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public int ViewPointState(int width, int height)
+        {
+            return visitedPoints[width, height];
+        }
+
+        public int ViewPointInfo(int width, int height)
+        {
+            return bombsAndValues[width, height];
+        }
+
+        public bool SelectPoint(int width, int height)
+        {
+            if (bombsAndValues[width, height] == -1) return false; //Return false if bomb hit
+            VisitNearbyPoints(width, height);
+            return true;
+        }
+
+        public bool ToggleFlag(int cellX, int cellY)
+        {
+            //Nothing occurs for a cell already visited
+
+            if (visitedPoints[cellX, cellY] == 0)
+            {
+                if (FlagsAvailable == 0) return false;
+                visitedPoints[cellX, cellY] = -1;
+                FlagsAvailable--;
+            }
+            else if (visitedPoints[cellX, cellY] == -1)
+            {
+                visitedPoints[cellX, cellY] = 0;
+                FlagsAvailable++;
+            }
+            return true;
+        }
+
+        //If a bomb is hit, do nothing
+        //If a non-zero cell is hit, only make it visited
+        //If a zero cell is hit, check all adjacent cells to see if any are also zero
+        //and branch out until all zeros that can be reached by the original zero are visited
+        void VisitNearbyPoints(int width, int height)
+        {
+            if (bombsAndValues[width, height] == -1) return; //Is a bomb
+            visitedPoints[width, height] = 1;
+            VisitedCells += 1;
+            if (bombsAndValues[width, height] > 0) return; //Don't reveal nearby of cells next to bombs
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int cellX = width + x;
+                    int cellY = height + y;
+                    if (cellX >= 0 && cellX < GridWidth && cellY >= 0 && cellY < GridHeight)
+                    {
+                        //Only call method on unvisited cells
+                        if (visitedPoints[cellX, cellY] == 0) VisitNearbyPoints(width + x, height + y);
+                    }
+                }
+            }
+        }
+
+        public bool Success()
+        {
+            return (GridWidth * GridHeight) == BombQuantity + VisitedCells;
+        }
+
+        public void RevealBombs()
+        {
+            for (int x = 0; x < GridWidth; x++)
+            {
+                for (int y = 0; y < GridHeight; y++)
+                {
+                    if (bombsAndValues[x, y] == -1) visitedPoints[x, y] = 1;
+                }
+            }
+        }
+    }
+}
--- MinesweeperBeta/MainPage.xaml.cs
+++ MinesweeperBeta/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MinesweeperBeta.Logic;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,30 +23,168 @@ namespace MinesweeperBeta
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        List<Button> cells = new List<Button>();
+        const int gameRows = 10;
+        const int gameColumns = 10;
+        const int gameBombs = 15;
+
+        Button[,] cells;
+        GameGrid game = new GameGrid(gameRows, gameColumns, gameBombs);
+
+        bool flagMode = false;
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            for (int i = 0; i < 4; i++)
+            cells = new Button[gameRows, gameColumns];
+
+            for (int i = 0; i < gameRows; i++)
             {
-                for (int j = 0; j < 4; j++)
+                PlayingField.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int j = 0; j < gameColumns; j++)
+            {
+                PlayingField.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < gameRows; i++)
+            {
+                for (int j = 0; j < gameColumns; j++)
                 {
-                    Button button = new Button
+
+                    CellButton button = new CellButton
                     {
-                        Content = "Button1",
+                        Margin = new Windows.UI.Xaml.Thickness(4.0),
                         HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch,
                         VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch,
                     };
+
+                    button.x = i;
+                    button.y = j;
+
+                    button.Click += new RoutedEventHandler(CellClickAsync);
+                    button.RightTapped += new RightTappedEventHandler(CellRightTap);
 
                     PlayingField.Children.Add(button);
                     Grid.SetRow(button, i);
                     Grid.SetColumn(button, j);
 
-                    cells.Add(button);
+                    cells[i, j] = button;
                 }
             }
+
+            Bombs_TextBlock.Text = "Bombs: " + gameBombs;
+            Flags_TextBlock.Text = "Flags Available: " + game.FlagsAvailable;
+
+            UpdateCells();
+        }
+
+        private void CellRightTap(Object sender, RightTappedRoutedEventArgs e)
+        {
+            CellButton b = (CellButton)sender;
+            game.ToggleFlag(b.x, b.y);
+            UpdateCells();
+            Flags_TextBlock.Text = "Flags Available: " + game.FlagsAvailable;
+            CheckIfWon();
+        }
+
+        private async void CellClickAsync(Object sender, RoutedEventArgs e)
+        {
+            CellButton b = (CellButton)sender;
+            if (game.ViewPointState(b.x, b.y) == -1) return;
+            bool notLost = game.SelectPoint(b.x, b.y);
+
+            var lostDialog = new ContentDialog
+            {
+                Title = "Game Over",
+                Content = "You've hit a mine!",
+                PrimaryButtonText = "Restart",
+                SecondaryButtonText = "Close"
+            };
+            UpdateCells();
+            if (!notLost)
+            {
+                game.RevealBombs();
+                UpdateCells();
+                var dialogReturn = await lostDialog.ShowAsync();
+                switch (dialogReturn)
+                {
+                    case ContentDialogResult.Primary:
+                        game.StartNewGame();
+                        UpdateCells();
+                        break;
+                    case ContentDialogResult.Secondary:
+                        break;
+                }
+            }
+            else CheckIfWon();
+        }
+
+        private void UpdateCells()
+        {
+            for (int i = 0; i < gameRows; i++)
+            {
+                for (int j = 0; j < gameColumns; j++)
+                {
+                    int cellVisitState = game.ViewPointState(i, j);
+                    int cellValue = game.ViewPointInfo(i, j);
+
+                    switch (cellVisitState)
+                    {
+                        case 1:
+                            switch (cellValue)
+                            {
+                                case -1:
+                                    cells[i, j].Content = "\uD83D\uDCA3";
+                                    break;
+                                case 0:
+                                    cells[i, j].Content = "";
+                                    break;
+                                default:
+                                    cells[i, j].Content = cellValue;
+                                    break;
+                            }
+                            cells[i, j].IsEnabled = false;
+                            break;
+                        case 0:
+                            cells[i, j].Content = "\u2217";
+                            break;
+                        case -1:
+                            cells[i, j].Content = "\u2691";
+                            break;
+                    }
+                }
+            }
+        }
+
+        public async void CheckIfWon()
+        {
+            if (game.Success())
+            {
+                var wonDialog = new ContentDialog
+                {
+                    Title = "Game Won",
+                    Content = "You've successfully avoided all bombs",
+                    PrimaryButtonText = "Restart",
+                    SecondaryButtonText = "Close"
+                };
+                var dialogReturn = await wonDialog.ShowAsync();
+                switch (dialogReturn)
+                {
+                    case ContentDialogResult.Primary:
+                        game.StartNewGame();
+                        UpdateCells();
+                        break;
+                    case ContentDialogResult.Secondary:
+                        break;
+                }
+            }
+        }
+
+        private class CellButton : Button
+        {
+            public int x;
+            public int y;
         }
     }
 }
