7686dfd5f50733770341519410e4492771686db1
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-08-10 11:33:27 +0100

    Add documentation - Document MainLogic and MainPage.cs - Change bomb generation method to reduce loops

--- MinesweeperBeta/Logic/MineLogic.cs
+++ MinesweeperBeta/Logic/MineLogic.cs
@@ -15,52 +15,78 @@ namespace MinesweeperBeta.Logic
 
         public int FlagsAvailable { get; private set; }
 
-        // -1: Bomb
-        //  0: Not a bomb, nor adjacent to a bomb
-        // >0: Adjacent to at least one bomb
-        private int[,] bombsAndValues;
+        /// <summary>
+        /// Matrix for whether cells is bomb or has other value.
+        /// </summary>
+        /// <remarks>
+        /// -1: Bomb
+        ///  0: Not a bomb, nor adjacent to a bomb
+        /// >0: Adjacent to at least one bomb
+        /// </remarks>
+        public int[,] BombsAndValues { get; private set; }
 
-        // -1: Flagged cell
-        //  0: Unvisited cell
-        //  1: Visited cell (once visited, cannot be flagged or unvisited)
-        private int[,] visitedPoints;
+        /// <summary>
+        /// Matrix for whether a cells is visited, unvisited or flagged
+        /// </summary>
+        /// <remarks>
+        /// -1: Flagged cell;
+        ///  0: Unvisited cell;
+        ///  1: Visited cell (once visited, cannot be flagged or unvisited)
+        /// </remarks>
+        public int[,] VisitedPoints { get; private set; }
 
-        public GameGrid(int width, int height, int bombQuantity)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameGrid"/> class.
+        /// </summary>
+        /// <param name="rows">
+        /// Number of rows in game.
+        /// </param>
+        /// <param name="columns">
+        /// Number of columns in game.
+        /// </param>
+        /// <param name="bombQuantity">
+        /// Number of bombs generated in game.
+        /// </param>
+        public GameGrid(int rows, int columns, int bombQuantity)
         {
             this.BombQuantity = bombQuantity;
             this.FlagsAvailable = bombQuantity;
-            this.GridWidth = width;
-            this.GridHeight = height;
+            this.GridWidth = rows;
+            this.GridHeight = columns;
 
             //Cannot proceed if there are meant to be more bombs than cells
             if (bombQuantity > (GridHeight * GridWidth)) throw new Exception();
 
             StartNewGame();
         }
-
+        
+        /// <summary>
+        /// Method resets board (all cells unvisited) and new bombs.
+        /// </summary>
         public void StartNewGame()
         {
-            bombsAndValues = new int[GridWidth, GridHeight];
-            visitedPoints = new int[GridWidth, GridHeight];
+            BombsAndValues = new int[GridWidth, GridHeight];
+            VisitedPoints = new int[GridWidth, GridHeight];
+            FlagsAvailable = BombQuantity;
+            VisitedCells = 0;
 
             Random bombRandom = new Random();
-            for (int i = 0; i < BombQuantity; i++)
+
+            var bombs = new List<int>();
+            while (bombs.Count < BombQuantity)
             {
-                //Since Random.Next() is min inclusive but max exclusive,
-                //only non-negative values less than gridWidth or gridHeight will be generated
-                int bombX = bombRandom.Next(0, GridWidth);
-                int bombY = bombRandom.Next(0, GridHeight);
+                int proposedBombLocation = bombRandom.Next(0, GridWidth * GridHeight);
+                if (!bombs.Contains(proposedBombLocation))
+                    bombs.Add(proposedBombLocation);
+            }
 
-                //Guarantees the number of bombs desired will appear
-                //and a bomb will not be placed in the same cell more
-                //than once
-                while (bombsAndValues[bombX, bombY] == -1)
-                {
-                    bombX = bombRandom.Next(0, GridWidth);
-                    bombY = bombRandom.Next(0, GridHeight);
-                }
+            foreach (int bombLocation in bombs)
+            {
+                int bombX = bombLocation / GridWidth;
+                int bombY = bombLocation % GridHeight;
 
-                bombsAndValues[bombX, bombY] = -1;
+                BombsAndValues[bombX, bombY] = -1;
+
                 for (int x = 0; x < 3; x++)
                 {
                     for (int y = 0; y < 3; y++)
@@ -68,89 +94,131 @@ namespace MinesweeperBeta.Logic
                         int positionX = bombX + x - 1;
                         int positionY = bombY + y - 1;
                         //Values must be in grid range (0 or gridWidth/Height cause issues)
-                        if (positionX <= GridWidth - 1 && positionY <= GridHeight - 1 && positionX >= 0 && positionY >= 0)
+                        if (positionX <= GridWidth - 1 &&
+                            positionY <= GridHeight - 1 &&
+                            positionX >= 0 && positionY >= 0)
                         {
                             //For all cells adjacent to a bomb that are not bombs,
                             //add one to the value of bombs that cell is adjacent to
-                            if (bombsAndValues[positionX, positionY] > -1) bombsAndValues[positionX, positionY] += 1;
+                            if (BombsAndValues[positionX, positionY] > -1)
+                                BombsAndValues[positionX, positionY] += 1;
                         }
                     }
                 }
             }
         }
 
-        public int ViewPointState(int width, int height)
+        /// <summary>
+        /// Select cell to reveal bomb or value.
+        /// </summary>
+        /// <param name="row">
+        /// Zero-indexed row of cell selected
+        /// </param>
+        /// <param name="column">
+        /// Zero-indexed column of cell selected
+        /// </param>
+        /// <returns>
+        /// False is bomb hit, otherwise true.
+        /// </returns>
+        public bool SelectPoint(int row, int column)
         {
-            return visitedPoints[width, height];
-        }
-
-        public int ViewPointInfo(int width, int height)
-        {
-            return bombsAndValues[width, height];
-        }
-
-        public bool SelectPoint(int width, int height)
-        {
-            if (bombsAndValues[width, height] == -1) return false; //Return false if bomb hit
-            VisitNearbyPoints(width, height);
+            //Return false if bomb hit
+            if (BombsAndValues[row, column] == -1) return false;
+            VisitNearbyPoints(row, column);
             return true;
         }
 
-        public bool ToggleFlag(int cellX, int cellY)
+        /// <summary>
+        /// Toggle flag for given cell.
+        /// </summary>
+        /// <param name="row">
+        /// Zero-indexed row of cell to be flagged.
+        /// </param>
+        /// <param name="column">
+        /// Zero-indexed column of cell to be flagged.
+        /// </param>
+        /// <returns>
+        /// False if no more flags available, otherwise true.
+        /// </returns>
+        /// <remarks>
+        /// Nothing occurs for a cell already visited.
+        /// </remarks>
+        public bool ToggleFlag(int row, int column)
         {
-            //Nothing occurs for a cell already visited
-
-            if (visitedPoints[cellX, cellY] == 0)
+            if (VisitedPoints[row, column] == 0)
             {
                 if (FlagsAvailable == 0) return false;
-                visitedPoints[cellX, cellY] = -1;
+                VisitedPoints[row, column] = -1;
                 FlagsAvailable--;
             }
-            else if (visitedPoints[cellX, cellY] == -1)
+            else if (VisitedPoints[row, column] == -1)
             {
-                visitedPoints[cellX, cellY] = 0;
+                VisitedPoints[row, column] = 0;
                 FlagsAvailable++;
             }
             return true;
         }
 
-        //If a bomb is hit, do nothing
-        //If a non-zero cell is hit, only make it visited
-        //If a zero cell is hit, check all adjacent cells to see if any are also zero
-        //and branch out until all zeros that can be reached by the original zero are visited
-        void VisitNearbyPoints(int width, int height)
+        /// <summary>
+        /// Propogates visiting cells after 0 value cell selected.
+        /// </summary>
+        /// <param name="row">
+        /// Zero-indexed row of cell.
+        /// </param>
+        /// <param name="column">
+        /// Zero-indexed column of cell.
+        /// </param>
+        /// <seealso cref="SelectPoint(int, int)"/>
+        /// <remarks>
+        /// If a bomb is hit, do nothing. 
+        /// If a non-zero cell is hit, only make it visited. 
+        /// If a zero cell is hit, check all adjacent cells to see if any are also zero
+        /// and branch out until all zeros that can be reached by the original zero are visited.
+        /// </remarks>
+
+        void VisitNearbyPoints(int row, int column)
         {
-            if (bombsAndValues[width, height] == -1) return; //Is a bomb
-            visitedPoints[width, height] = 1;
+            if (BombsAndValues[row, column] == -1) return; //Is a bomb
+            VisitedPoints[row, column] = 1;
             VisitedCells += 1;
-            if (bombsAndValues[width, height] > 0) return; //Don't reveal nearby of cells next to bombs
+            if (BombsAndValues[row, column] > 0) return; //Don't reveal nearby of cells next to bombs
             for (int x = -1; x < 2; x++)
             {
                 for (int y = -1; y < 2; y++)
                 {
-                    int cellX = width + x;
-                    int cellY = height + y;
+                    int cellX = row + x;
+                    int cellY = column + y;
                     if (cellX >= 0 && cellX < GridWidth && cellY >= 0 && cellY < GridHeight)
                     {
                         //Only call method on unvisited cells
-                        if (visitedPoints[cellX, cellY] == 0) VisitNearbyPoints(width + x, height + y);
+                        if (VisitedPoints[cellX, cellY] == 0) VisitNearbyPoints(row + x, column + y);
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Method determines whether user has won the game.
+        /// </summary>
+        /// <returns>
+        /// Returns true when a non-bomb cells visited, otherwise
+        /// returns false.
+        /// </returns>
         public bool Success()
         {
             return (GridWidth * GridHeight) == BombQuantity + VisitedCells;
         }
 
+        /// <summary>
+        /// Makes all bomb cells visited.
+        /// </summary>
         public void RevealBombs()
         {
             for (int x = 0; x < GridWidth; x++)
             {
                 for (int y = 0; y < GridHeight; y++)
                 {
-                    if (bombsAndValues[x, y] == -1) visitedPoints[x, y] = 1;
+                    if (BombsAndValues[x, y] == -1) VisitedPoints[x, y] = 1;
                 }
             }
         }
--- MinesweeperBeta/MainPage.xaml.cs
+++ MinesweeperBeta/MainPage.xaml.cs
@@ -19,25 +19,43 @@ using MinesweeperBeta.Logic;
 namespace MinesweeperBeta
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Page showing the playing field, game title, bombs and flags available.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        const int gameRows = 10;
-        const int gameColumns = 10;
-        const int gameBombs = 15;
+        /// <summary>
+        /// Fixed number of rows on playing field.
+        /// </summary>
+        private const int gameRows = 12;
+        /// <summary>
+        /// Fixed number of columns on the playing field.
+        /// </summary>
+        private const int gameColumns = 12;
+        /// <summary>
+        /// Fixed number of bombs used in the game.
+        /// </summary>
+        private const int gameBombs = 20;
 
-        Button[,] cells;
-        GameGrid game = new GameGrid(gameRows, gameColumns, gameBombs);
+        /// <summary>
+        /// Matrix of cells represented by buttons on the field.
+        /// </summary>
+        private Button[,] cells;
+        /// <summary>
+        /// Underlying logic of the game using <see cref="GameGrid"/>.
+        /// </summary>
+        private GameGrid game = new GameGrid(gameRows, gameColumns, gameBombs);
 
-        bool flagMode = false;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
 
             cells = new Button[gameRows, gameColumns];
 
+            //Adds one row definition per gameRow and column definition
+            //per gameColumn
             for (int i = 0; i < gameRows; i++)
             {
                 PlayingField.RowDefinitions.Add(new RowDefinition());
@@ -51,7 +69,8 @@ namespace MinesweeperBeta
             {
                 for (int j = 0; j < gameColumns; j++)
                 {
-
+                    //Each button uses ButtonRevealStyle, has margin 4 all
+                    //around and stretches to fill the grid spot it is in
                     CellButton button = new CellButton
                     {
                         Margin = new Windows.UI.Xaml.Thickness(4.0),
@@ -74,24 +93,59 @@ namespace MinesweeperBeta
             }
 
             Bombs_TextBlock.Text = "Bombs: " + gameBombs;
-            Flags_TextBlock.Text = "Flags Available: " + game.FlagsAvailable;
+            UpdateFlagsTextBlock();
 
             UpdateCells();
         }
 
+        /// <summary>
+        /// Updates <see cref="Flags_TextBlock"/> with current number of flags
+        /// still available.
+        /// </summary>
+        private void UpdateFlagsTextBlock()
+        {
+            Flags_TextBlock.Text = "Flags Available: " + game.FlagsAvailable;
+        }
+
+        /// <summary>
+        /// Handles cell (button) right click or hold and release.
+        /// </summary>
+        /// <param name="sender">
+        /// The cell (CellButton) upon which the selection was performed.
+        /// </param>
+        /// <param name="e">
+        /// <see cref="RightTappedRoutedEventArgs"/> for associated
+        /// <see cref="RightTappedEventHandler"/>
+        /// </param>
+        /// <remarks>
+        /// Right click signifies toggling the flag.
+        /// </remarks>
         private void CellRightTap(Object sender, RightTappedRoutedEventArgs e)
         {
             CellButton b = (CellButton)sender;
             game.ToggleFlag(b.x, b.y);
             UpdateCells();
-            Flags_TextBlock.Text = "Flags Available: " + game.FlagsAvailable;
+            UpdateFlagsTextBlock();
             CheckIfWon();
         }
 
+        /// <summary>
+        /// Handles cell (button) click or tap.
+        /// </summary>
+        /// <param name="sender">
+        /// The cell (CellButton) upon which the selection was performed.
+        /// </param>
+        /// <param name="e">
+        /// <see cref="RoutedEventArgs"/> for associated
+        /// <see cref="RoutedEvent"/>
+        /// </param>
         private async void CellClickAsync(Object sender, RoutedEventArgs e)
         {
             CellButton b = (CellButton)sender;
-            if (game.ViewPointState(b.x, b.y) == -1) return;
+
+            //If cell flagged, do nothing
+            if (game.VisitedPoints[b.x, b.y] == -1) return;
+
             bool notLost = game.SelectPoint(b.x, b.y);
 
             var lostDialog = new ContentDialog
@@ -102,6 +156,9 @@ namespace MinesweeperBeta
                 SecondaryButtonText = "Close"
             };
             UpdateCells();
+
+            //If selection performed on bomb, reveal bombs
+            //and allow the user to restart or close the dialog
             if (!notLost)
             {
                 game.RevealBombs();
@@ -109,25 +166,29 @@ namespace MinesweeperBeta
                 var dialogReturn = await lostDialog.ShowAsync();
                 switch (dialogReturn)
                 {
-                    case ContentDialogResult.Primary:
+                    case ContentDialogResult.Primary: //Restart button
                         game.StartNewGame();
                         UpdateCells();
+                        foreach (CellButton cb in cells) cb.IsEnabled = true;
                         break;
-                    case ContentDialogResult.Secondary:
+                    case ContentDialogResult.Secondary: //Close button
                         break;
                 }
             }
             else CheckIfWon();
         }
 
+        /// <summary>
+        /// Updates text of cells based on their value.
+        /// </summary>
         private void UpdateCells()
         {
             for (int i = 0; i < gameRows; i++)
             {
                 for (int j = 0; j < gameColumns; j++)
                 {
-                    int cellVisitState = game.ViewPointState(i, j);
-                    int cellValue = game.ViewPointInfo(i, j);
+                    int cellVisitState = game.VisitedPoints[i, j];
+                    int cellValue = game.BombsAndValues[i, j];
 
                     switch (cellVisitState)
                     {
@@ -135,21 +196,27 @@ namespace MinesweeperBeta
                             switch (cellValue)
                             {
                                 case -1:
+                                    //Bomb emoji
                                     cells[i, j].Content = "\uD83D\uDCA3";
                                     break;
                                 case 0:
+                                    //No text for 0 value cells
                                     cells[i, j].Content = "";
                                     break;
                                 default:
+                                    //Cell value which is at least 1.
                                     cells[i, j].Content = cellValue;
                                     break;
                             }
+                            //Disable cells that are visited
                             cells[i, j].IsEnabled = false;
                             break;
                         case 0:
+                            //Mathematic asterix
                             cells[i, j].Content = "\u2217";
                             break;
                         case -1:
+                            //Flag symbol
                             cells[i, j].Content = "\u2691";
                             break;
                     }
@@ -157,6 +224,9 @@ namespace MinesweeperBeta
             }
         }
 
+        /// <summary>
+        /// Displays dialog if game won.
+        /// </summary>
         public async void CheckIfWon()
         {
             if (game.Success())
@@ -181,6 +251,9 @@ namespace MinesweeperBeta
             }
         }
 
+        /// <summary>
+        /// Button with reference to its location on grid.
+        /// </summary>
         private class CellButton : Button
         {
             public int x;
